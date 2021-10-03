f371fca83e6a7b499a88ff305b7b1c0fbe35faec
TonyMoon <tonycontinue@gmail.com>
2021-10-02 22:32:58 -0700

    Time: 172 ms (55.51%), Space: 15.6 MB (10.71%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,4 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        cnt = collections.Counter(nums)
-        return max(cnt.keys(), key=cnt.get)
+        
+        #Boyer-Moore Voting Algorithm
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+            
+        return candidate
+        
+        
+        
+        
+        # cnt = collections.Counter(nums)
+        # return max(cnt.keys(), key=cnt.get)
